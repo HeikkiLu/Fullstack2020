4ff0d414f0b80f062044b7212f9b0318b1bab21a
Heikki <heikki.luukkonen@gmail.com>
2022-08-28 11:45:35 +0300

    5.12 done

--- osa5/bloglist-frontend/src/components/CreateBlogForm.js
+++ osa5/bloglist-frontend/src/components/CreateBlogForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const CreateBlogForm = ({ addBlog, newTitle, newAuthor, newUrl, handleAuthorChange, handleTitleChange, handleUrlChange }) => {
   return (
--- osa5/bloglist-frontend/src/components/Notification.js
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -1,10 +1,10 @@
 const Notification = ({ message, type }) => {
   if (message === null) return null
   return (
-      <div className={type}>
-            {message} 
-      </div>
-  );
-};
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
 
 export default Notification
